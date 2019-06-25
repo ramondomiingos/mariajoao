37f0c82394dd68d90a6477c4fa1f70c9fae2b5b8
Ramon Domingos <ramongemio@gmail.com>
2019-06-25 15:29:00 -0300

    menu dashborad login sem servico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+                        { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+                        { path: 'dashboard', component: DashboardComponent},
+                        { path: 'login', component: LoginComponent}
+                       ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
